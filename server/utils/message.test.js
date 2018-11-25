39b22a8e4530b8279ff4e24cc726184f43db8d4e
sai abhinav <saiabhinavvasepalli@gmail.com>
2018-11-25 13:40:53 +0530

    Create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,15 @@
+const expect = require('expect');
+
+var { generateMessage } = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate the correct message object', () => {
+        var from = 'Abhinav';
+        var text = 'Some message';
+        var message = generateMessage(from, text);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect(message.from).toBe(from);
+        expect(message.text).toBe(text);
+    });
+});
