ff824f722a7bbd80a5423f475e4aff4319dcb558
sai abhinav <saiabhinavvasepalli@gmail.com>
2018-11-26 12:15:43 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,23 +29,32 @@ socket.on('newLocationMessage', function(message) {
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
-    }, function () {});
+        text: messageTextbox.val()
+    }, function () {
+        messageTextbox.val('');
+    });
 });
 
-var locationButton = $('#send-location').on('click', function () {
+var locationButton = $('#send-location')
+locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     });
 });
